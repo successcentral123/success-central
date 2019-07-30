b5a2bb9b993679b2ca42b159960556bcde0d0070
Phil Murray <philipmurray80@gmail.com>
2019-07-29 20:20:31 -0400

    First attempt at converting project to Maven structure - because App Engine likes that.

---
+++ src/main/java/com/ccsu/cs530/successcentral/util/DatabaseConnection.java
@@ -0,0 +1,82 @@
+package com.ccsu.cs530.successcentral.util;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.*;
+
+public class DatabaseConnection {
+
+    private DataSource ds;
+    private static Connection con;
+    private PreparedStatement pr;
+    private InitialContext ic;
+
+    public static Connection getConnection() {
+        if (con != null) {
+            return con;
+        }
+
+        try {
+            HikariConfig config = new HikariConfig();
+
+            // Configure which instance and what database user to connect with.
+            config.setJdbcUrl("jdbc:google:mysql://success-central:us-east4:success-central-db/success_central?user=root");
+
+            DataSource pool = new HikariDataSource(config);
+            con = pool.getConnection();
+            return con;
+
+        } catch (Exception e) {
+            System.out.println("Could not connect Booya");
+            return null;
+        }
+//        try {
+//            return ((DataSource) InitialContext.doLookup("java:/SCDB")).getConnection();
+//        } catch (Exception e) {
+//            System.out.println("Hey CCSU team. Could not connect to DB");
+//            return null;
+//        }
+    }
+
+    public void open() throws NamingException, SQLException {
+       // The configuration object specifies behaviors for the connection pool.
+        HikariConfig config = new HikariConfig();
+
+        // Configure which instance and what database user to connect with.
+        config.setJdbcUrl("jdbc:google:mysql://success-central:us-east4:success-central-db/success_central?user=root");
+
+
+        // Initialize the connection pool using the configuration object.
+        DataSource pool = new HikariDataSource(config);
+        con = pool.getConnection();
+
+//        ic = new InitialContext();
+//        ds = (DataSource) ic.lookup("java:/SCDB");
+//        con = ds.getConnection();
+    }
+
+    public void close() throws SQLException {
+        if (pr != null){
+            pr.close();
+        }
+        if (con != null) {
+            con.close();
+        }
+    }
+
+    public int executeUpdateCommand(String command) throws SQLException {
+        pr = con.prepareStatement(command);
+        int i = pr.executeUpdate();
+        return i;
+    }
+
+    public ResultSet executeQueryCommand(String command) throws SQLException {
+        pr = con.prepareStatement(command);
+        ResultSet rs = pr.executeQuery();
+        return rs;
+    }
+
+}
---
+++ src/test/java/com/ccsu/cs530/successcentral/RegistrationAndLoginTest.java
@@ -0,0 +1,21 @@
+package com.ccsu.cs530.successcentral;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import javax.servlet.http.*;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+
+public class RegistrationAndLoginTest extends Mockito {
+
+    @Test
+    public void testRegister() {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+
+
+    }
+}
