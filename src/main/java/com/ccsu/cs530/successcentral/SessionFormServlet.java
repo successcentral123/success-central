5a277d9592c6db6bc17c38dd7733dfc7979ab52f
successcentral123@gmail.com <ehfltkah21>
2019-11-03 04:14:12 -0500

    uncommented the doPost

--- src/main/java/com/ccsu/cs530/successcentral/SessionFormServlet.java
+++ src/main/java/com/ccsu/cs530/successcentral/SessionFormServlet.java
@@ -1,6 +1,6 @@
 package com.ccsu.cs530.successcentral;
 
-import com.ccsu.cs530.successcentral.model.Mentor;
+import com.ccsu.cs530.successcentral.model.SessionForm;
 import com.ccsu.cs530.successcentral.service.CrudService;
 
 import javax.servlet.ServletException;
@@ -18,7 +18,7 @@ public class SessionFormServlet extends HttpServlet {
     private CrudService crud = new CrudService();
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // todo Need to set attributes for Previous Action Steps
 
 //        SessionForm sessform = new SessionForm();
@@ -31,148 +31,86 @@ public class SessionFormServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 
         //todo these wont work until model and crud work
-//        SessionForm sessionform = new SessionForm();
-//        if (req.getParameter("firstname") != null) {
-//            sessionform.setFirstName(req.getParameter("firstname"));
-//        }
-//        if (req.getParameter("lastname") != null) {
-//            sessionform.setLastName(req.getParameter("lastname"));
-//        }
-//        if (req.getParameter("sessionnum") != null) {
-//            sessionform.setSessionNum(req.getParameter("sessionnum"));
-//        }
-//        if (req.getParameter("date") != null) {
-//            sessionform.setDate(req.getParameter("date"));
-//        }
-//
-//        //TODO the preaction steps. decided to send whole array and do the assignment in the model
-//        if (req.getParameter("preactionsteps") != null) {
-//            String[] preActionSteps = req.getParameterValues("preactionsteps");
-//            sessionform.setPreActionSteps(preActionSteps);;
-//
-//            //todo this is the assignmet, should be in the model
-//            String preAction = "";
-//            for (int i = 0; i < preActionSteps.length; i++) {
-//                preAction = preActionSteps[i];
-//                switch (i) {
-//                    case 1:
-//                        sessionform.setPreActionOne(preAction);
-//                        sessionform.setBoolActionOne(true);
-//                        break;
-//                    case 2:
-//                        sessionform.setPreActionTwo(preAction);
-//                        sessionform.setBoolActionTwo(true);
-//                        break;
-//                    case 3:
-//                        sessionform.setPreActionThree(preAction);
-//                        sessionform.setBoolActionThree(true);
-//                        break;
-//                    case 4:
-//                        sessionform.setPreActionFour(preAction);
-//                        sessionform.setBoolActionFour(true);
-//                        break;
-//                    case 5:
-//                        sessionform.setPreActionFive(preAction);
-//                        sessionform.setBoolActionFive(true);
-//                        break;
-//                    case 6:
-//                        sessionform.setPreActionSix(preAction);
-//                        sessionform.setBoolActionSix(true);
-//                        break;
-//                    default:
-//                        break;
-//                }
-//            }
-//        }
+        SessionForm sessionform = new SessionForm();
+        if (req.getParameter("firstname") != null) {
+            sessionform.setFirstName(req.getParameter("firstname"));
+        }
+        if (req.getParameter("lastname") != null) {
+            sessionform.setLastName(req.getParameter("lastname"));
+        }
+        if (req.getParameter("sessionnum") != null) {
+            sessionform.setSessionNum(Integer.parseInt(req.getParameter("sessionnum")));
+        }
+        if (req.getParameter("date") != null) {
+            sessionform.setDate(req.getParameter("date"));
+        }
 
-//        if (req.getParameter("scale") != null) {
-//            sessionform.setScale(req.getParameter("scale"));
-//        }
-//
-//        //TODO session topics may not work. decided to send whole array and do the assignment in the model
-//        if (req.getParameter("sessiontopics") != null) {
-//            String[] topicsFromForm = req.getParameterValues("sessiontopics");
-//            sessionform.setTopicsForm(topicsFromForm);
+        //TODO the preaction steps. decided to send whole array and do the assignment in the model
+        if (req.getParameter("preactionsteps") != null) {
+            String[] preActionSteps = req.getParameterValues("preactionsteps");
+            sessionform.setPreActionSteps(preActionSteps);
+        }
 
-//            //todo this is the assignment that should be in the model
-//            String topicsString = "";
-//            for (int i = 0; i < topicsFromForm.length; i++){
-//                topicsString = topicsFromForm[i];
-                    //todo take this out and put it in model
-//                switch (topicsString) {
-//                    case "Campus Involvement":
-//                        seesionform.setCampusInvolement(true);
-//                        break;
-//                    case "Meaningful Relationships":
-//                        sessionform.setMeaningfulRelationships(true);
-//                        break;
-//                    case "Financial Management":
-//                        sessionform.setFinancialManagement(true);
-//                        break;
-//                    case "Outside Responsibilities":
-//                        sessionform.setOutsideResponsibilities(true);
-//                        break;
-//                    case "Study Skills/Time Management":
-//                        sessionform.setStudyTimeManagement(true);
-//                        break;
-//                    case "Academic Engagement":
-//                        sessionform.setAcademicEngagement(true);
-//                        break;
-//                    case "Health & Wellness":
-//                        sessionform.setHealthWellness(true);
-//                        break;
-//                    case "Other":
-//                        sessionform.setOther(true);
-//                        sessionform.setOtherText(req.getParameter("othersesstopic"));
-//                        break;
-//                    default:
-//                        break;
-//                }
-//            }
+        if (req.getParameter("scale") != null) {
+            sessionform.setScale(Integer.parseInt(req.getParameter("scale")));
+        }
+
+        // gets array of all the checked topics
+        if (req.getParameter("sessiontopics") != null) {
+            String[] topicsFromForm = req.getParameterValues("sessiontopics");
+            sessionform.setTopicsForm(topicsFromForm);
+
+            // sets the other text box topic
+            String topicsString = "";
+            for (int i = 0; i < topicsFromForm.length; i++) {
+                topicsString = topicsFromForm[i];
+                switch (topicsString) {
+                    case "Other":
+                        sessionform.setOtherText(req.getParameter("othersesstopic"));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        if (req.getParameter("issues_concerns") != null) {
+            sessionform.setIssuesConcerns(req.getParameter("issues_concerns"));
+        }
+        if (req.getParameter("notes_comments") != null) {
+            sessionform.setNotesComments(req.getParameter("notes_comments"));
+        }
+        if (req.getParameter("firstactionstep") != null) {
+            sessionform.setFirstActionStep(req.getParameter("firstactionstep"));
+        }
+        if (req.getParameter("secondactionstep") != null) {
+            sessionform.setSecondActionStep(req.getParameter("secondactionstep"));
+        }
+        if (req.getParameter("thirdactionstep") != null) {
+            sessionform.setThirdActionStep(req.getParameter("thirdactionstep"));
+        }
+        if (req.getParameter("fourthactionstep") != null) {
+            sessionform.setFourthActionStep(req.getParameter("fourthactionstep"));
+        }
+        if (req.getParameter("fifthactionstep") != null) {
+            sessionform.setFifthActionStep(req.getParameter("fifthactionstep"));
+        }
+        if (req.getParameter("sixthtactionstep") != null) {
+            sessionform.setSixthActionStep(req.getParameter("sixthtactionstep"));
+        }
+            // if the user already exists, update 'em
+//        if (crud.getSessionForm(mentor.getEmail()).getEmail() != null) {
+//            crud.updateUser(mentor);
+//        } else {
+//            crud.createUser(mentor);
 //        }
-//
-//        if (req.getParameter("issues_concerns") != null) {
-//            sessionform.setIssuesConcerns(req.getParameter("issues_concerns"));
-//        }
-//        if (req.getParameter("notes_comments") != null) {
-//            sessionform.setNotesComments(req.getParameter("notes_comments"));
-//        }
-//        if (req.getParameter("firstactionstep") != null) {
-//            sessionform.setFirstActionStep(req.getParameter("firstactionstep"));
-//        }
-//        if (req.getParameter("secondactionstep") != null) {
-//            sessionform.setSecondActionStep(req.getParameter("secondactionstep"));
-//        }
-//        if (req.getParameter("thirdactionstep") != null) {
-//            sessionform.setThirdActionStep(req.getParameter("thirdactionstep"));
-//        }
-//        if (req.getParameter("fourthactionstep") != null) {
-//            sessionform.setFourthActionStep(req.getParameter("fourthactionstep"));
-//        }
-//        if (req.getParameter("fifthactionstep") != null) {
-//            sessionform.setFifthActionStep(req.getParameter("fifthactionstep"));
-//        }
-//        if (req.getParameter("sixthtactionstep") != null) {
-//            sessionform.setSixthActionStep(req.getParameter("sixthtactionstep"));
-//
-//
-//
-//
-//
-//        // if the user already exists, update 'em
-////        if (crud.getSessionForm(mentor.getEmail()).getEmail() != null) {
-////            crud.updateUser(mentor);
-////        } else {
-////            crud.createUser(mentor);
-////        }
-//        crud.createSessionForm(sessionform);
-////            message = "<p class=\"text-success border border-success\">Mentor successfully created!</p>";
-////        req.setAttribute("mentor", "true");
-////        req.setAttribute("email", mentor.getEmail());
-        req.getRequestDispatcher("form_success").forward(req, resp);
+            crud.createSessionForm(sessionform);
+//            message = "<p class=\"text-success border border-success\">Mentor successfully created!</p>";
+//        req.setAttribute("mentor", "true");
+//        req.setAttribute("email", mentor.getEmail());
+            req.getRequestDispatcher("form_success").forward(req, resp);
+
+        }
+
 
     }
-
-
-
-}
