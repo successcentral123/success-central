0c4c6bd195616bf6f2eb020e8607ded50abc6bdf
clarkpeterson <cdpeterson@my.ccsu.edu>
2019-11-07 23:14:56 -0500

    Changed name of jsp to report_intake

--- src/main/java/com/ccsu/cs530/successcentral/ReportChoiceServlet.java
+++ src/main/java/com/ccsu/cs530/successcentral/ReportChoiceServlet.java
@@ -1,6 +1,5 @@
 package com.ccsu.cs530.successcentral;
 
-import com.ccsu.cs530.successcentral.model.Mentee;
 import com.ccsu.cs530.successcentral.service.CrudService;
 
 import javax.servlet.ServletException;
@@ -9,17 +8,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
-@WebServlet("/report_choice")
+@WebServlet("/report_intake")
 public class ReportChoiceServlet extends HttpServlet {
     private CrudService crud = new CrudService();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-        req.getRequestDispatcher("report_choice.jsp").forward(req, resp);
+        if (req.getSession().getAttribute("email") != null && req.getSession().getAttribute("isAdmin").equals("true")) {
+            req.getRequestDispatcher("report_intake.jsp").forward(req, resp);
+        }
     }
 
     @Override
