b11ccbc23f3d03e5251e59dd72e6a703a2545f7f
JimmyPhas <JimmyPhasouk@gmail.com>
2019-11-20 21:26:44 -0500

    now send a session for to jsp

--- src/main/java/com/ccsu/cs530/successcentral/SessionFormInfoServlet.java
+++ src/main/java/com/ccsu/cs530/successcentral/SessionFormInfoServlet.java
@@ -1,8 +1,6 @@
 package com.ccsu.cs530.successcentral;
 
 import com.ccsu.cs530.successcentral.model.SessionForm;
-import com.ccsu.cs530.successcentral.model.Mentor;
-import com.ccsu.cs530.successcentral.model.Mentee;
 import com.ccsu.cs530.successcentral.service.CrudService;
 
 import javax.servlet.ServletException;
@@ -11,134 +9,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 @WebServlet("/session_form_info")
 public class SessionFormInfoServlet extends HttpServlet {
     private CrudService crud = new CrudService();
 
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // todo want to set attributes for Previous Action Steps by pulling from database
-        // todo this does not work
-        req.setAttribute("myMentees", crud.getMyMentees((String)req.getSession().getAttribute("email")));
-        req.getRequestDispatcher("session_form_info.jsp").forward(req, resp);
+        if (req.getSession().getAttribute("email") != null) {
+            String firstname = req.getParameter("firstName");
+            String lastname = req.getParameter("lastName");
+            int sessionNum = Integer.parseInt(req.getParameter("seshNum"));
+
+
+            SessionForm sessionform = crud.getSessionForm(firstname, lastname, sessionNum);
+
+            // populate the request object to send to the jsp
+            req.setAttribute("sessionform", sessionform);
+
+            req.getRequestDispatcher("session_form_info.jsp").forward(req, resp);
+        } else {
+            resp.sendRedirect("login");
+        }
     }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-
-        String mentorEmail = (String)req.getSession().getAttribute("email");
-        Mentor mentor = crud.getMentor(mentorEmail);
-        String mentorFirst = mentor.getFirstName();
-        String mentorLast = mentor.getLastName();
-        // Instantiate sessionform and then it to database
-        SessionForm sessionform = new SessionForm();
-        sessionform.setMentor(mentor);
-        List<Mentee> menteelist = new ArrayList<>();
-        menteelist = crud.getMyMentees(mentorEmail);
-
-        if (req.getParameter("firstname") != null) {
-
-            sessionform.setFirstName(req.getParameter("firstname"));
-        }
-        if (req.getParameter("lastname") != null) {
-            sessionform.setLastName(req.getParameter("lastname"));
-        }
-        if (req.getParameter("sessionnum") != null) {
-            sessionform.setSessionNum(Integer.parseInt(req.getParameter("sessionnum")));
-        }
-        if (req.getParameter("date") != null) {
-            sessionform.setDate(req.getParameter("date"));
-        }
-
-        //sends the check box values as an array
-        if (req.getParameter("prevactionsteps") != null) {
-            String[] boolPreAction = req.getParameterValues("prevactionsteps");
-            sessionform.setPreActionSteps(boolPreAction);
-        }
-        // sets the input of the input boxes
-        if (req.getParameter("preactionone") != null) {
-            sessionform.setPreActionOne(req.getParameter("preactionone"));
-        }
-        if (req.getParameter("preactiontwo") != null) {
-            sessionform.setPreActionTwo(req.getParameter("preactiontwo"));
-        }
-        if (req.getParameter("preactionthree") != null) {
-            sessionform.setPreActionThree(req.getParameter("preactionthree"));
-        }
-        if (req.getParameter("preactionfour") != null) {
-            sessionform.setPreActionFour(req.getParameter("preactionfour"));
-        }
-        if (req.getParameter("preactionfive") != null) {
-            sessionform.setPreActionFive(req.getParameter("preactionfive"));
-        }
-        if (req.getParameter("preactionsix") != null) {
-            sessionform.setPreActionSix(req.getParameter("preactionsix"));
-        }
-
-        if (req.getParameter("scale") != null) {
-            sessionform.setScale(Integer.parseInt(req.getParameter("scale")));
-        }
-
-        // gets array of all the checked topics
-        if (req.getParameter("sessiontopics") != null) {
-            String[] topicsFromForm = req.getParameterValues("sessiontopics");
-            sessionform.setTopicsForm(topicsFromForm);
-
-            // sets the other text box topic
-            String topicsString = "";
-            for (int i = 0; i < topicsFromForm.length; i++) {
-                topicsString = topicsFromForm[i];
-                switch (topicsString) {
-                    case "Other":
-                        sessionform.setOtherText(req.getParameter("othersesstopic"));
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-
-        if (req.getParameter("issues_concerns") != null) {
-            sessionform.setIssuesConcerns(req.getParameter("issues_concerns"));
-        }
-        if (req.getParameter("notes_comments") != null) {
-            sessionform.setNotesComments(req.getParameter("notes_comments"));
-        }
-        if (req.getParameter("firstactionstep") != null) {
-            sessionform.setFirstActionStep(req.getParameter("firstactionstep"));
-        }
-        if (req.getParameter("secondactionstep") != null) {
-            sessionform.setSecondActionStep(req.getParameter("secondactionstep"));
-        }
-        if (req.getParameter("thirdactionstep") != null) {
-            sessionform.setThirdActionStep(req.getParameter("thirdactionstep"));
-        }
-        if (req.getParameter("fourthactionstep") != null) {
-            sessionform.setFourthActionStep(req.getParameter("fourthactionstep"));
-        }
-        if (req.getParameter("fifthactionstep") != null) {
-            sessionform.setFifthActionStep(req.getParameter("fifthactionstep"));
-        }
-        if (req.getParameter("sixthactionstep") != null) {
-            sessionform.setSixthActionStep(req.getParameter("sixthactionstep"));
-        }
-
-
-//        message = "<p class=\"text-success border border-success\">Session form successfully created!</p>";
-
-        crud.createSessionForm(sessionform);
-        // sets form type to seesion_form so form_success can show correct page message
-        // todo want to change the attribute "mentor" to "form_type"
-        req.setAttribute("mentor", "session_form");
-        req.getRequestDispatcher("form_success").forward(req, resp);
-
-    }
-
-
 }
